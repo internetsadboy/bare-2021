1dd47602280c54add93695fa24714ba141436840
isb <jaredlamont90@gmail.com>
2021-02-23 07:08:42 -0800

    Added: functional mobile nav, responsive style adjustments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(function(){
   $(document).ready(function() {
+    $('.nav__icon').on('click', function() {
+  		$(this).toggleClass('active');
+  		$('nav ul').slideToggle();
+  	});
+
+  	$(window).on('load resize',function(){
+  		var w = $(window).width();
+  		var h = $(window).height();
+  		var x = 700;
+  		if (w >= x) {
+  			$('nav ul').css({ display: 'flex', height: 'auto' });
+  		} else {
+  			$('nav ul').css({ display: 'none', height: h - 300 + 'px'});
+  		}
+  	});
+  })
 });
